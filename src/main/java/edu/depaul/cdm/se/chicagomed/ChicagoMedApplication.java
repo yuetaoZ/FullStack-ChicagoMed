4e701726b160c41ac48556efcc4dc31295c23850
Kevin Looft <kdlooft@gmail.com>
2020-04-07 15:27:03 -0500

    Adding Spring framework

---
+++ src/main/java/edu/depaul/cdm/se/chicagomed/ChicagoMedApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se.chicagomed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicagoMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicagoMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/depaul/cdm/se/chicagomed/ChicagoMedApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se.chicagomed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicagoMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
