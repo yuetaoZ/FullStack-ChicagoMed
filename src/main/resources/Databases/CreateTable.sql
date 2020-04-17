b90e16f5377d97eaf620cb072ed5f35eb068b3c6
ArbaazKhan1 <bazzy_94@yahoo.com>
2020-04-17 15:54:19 -0500

    Added Patient & Location Tables

--- src/main/resources/Databases/CreateTable.sql
+++ src/main/resources/Databases/CreateTable.sql
@@ -5,3 +5,37 @@ Create Table appointment (
     doctorId integer,
     locationId integer
 );
+
+CREATE TABLE patient (
+    patientId integer  PRIMARY KEY ,
+    patientFirstName varchar (255),
+    patientLastName varchar (255)
+);
+
+CREATE TABLE patientDOB (
+    patientId integer  PRIMARY KEY,
+    dateOfBirth data
+);
+
+CREATE TABLE patientContact (
+    patientId integer PRIMARY KEY,
+    email varchar (255),
+    phoneNumber varchar (255),
+    address varchar (255),
+    city varchar (255),
+    state varchar (255),
+    zipCode integer
+);
+
+CREATE TABLE location (
+    locationId int PRIMARY KEY,
+    address varchar (255),
+    city varchar (255),
+    state varchar (255),
+    zipCode integer
+);
+
+CREATE TABLE locationDoc (
+    locationId integer PRIMARY KEY,
+    doctorId integer 
+);
