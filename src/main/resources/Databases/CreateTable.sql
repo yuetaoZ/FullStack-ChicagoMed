7d931dfc47468ea2d9cb0d67ad327e60742b4cc8
Tommy O'Connell <thomas.m.oconnell@gmail.com>
2020-04-17 16:01:11 -0500

    Updated with Doctor, Bill, and Appointment tables

--- src/main/resources/Databases/CreateTable.sql
+++ src/main/resources/Databases/CreateTable.sql
@@ -1,7 +1,35 @@
-Create Table appointment (
-    appointmentId integer PRIMARY KEY,
-    appointmentDate data,
-    patientId integer,
-    doctorId integer,
-    locationId integer
+CREATE TABLE public.appointment(
+    appointmentid int PRIMARY KEY,
+    appointmentdate date,
+    patientid int,
+    doctorid int,
+    locationid int
+    );
+
+CREATE TABLE public.bill(
+    appointmentid integer PRIMARY KEY,
+    billid int,
+    billamt int,
+    paidamt int,
+    duedate date,
+	patientid int,
+	billbalance int
+);
+
+CREATE TABLE public.doctor(
+    doctorid int PRIMARY KEY,
+    doctorfirstname varchar(255),
+    doctorlastname varchar(255)
+);
+
+
+CREATE TABLE public.doctorcontact(
+    doctorid int PRIMARY KEY,
+    doctoremail varchar(255),
+    doctorphone varchar(255)
+);
+
+CREATE TABLE public.doctorspecialization(
+    doctorid int PRIMARY KEY,
+    specialization varchar(255)
 );
