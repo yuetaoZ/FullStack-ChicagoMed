25a57dc58b5979bf1c95467e3b537635a0315ded
Tommy O'Connell <thomas.m.oconnell@gmail.com>
2020-04-25 18:42:49 -0500

    Updated doctor and location java

---
+++ src/main/java/edu/depaul/cdm/se/chicagomed/model/Doctor.java
@@ -0,0 +1,33 @@
+package edu.depaul.cdm.se.chicagomed.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "doctor")
+@ToString
+public class Doctor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @Column(unique = true)
+    @Size(max = 5)
+    private String doctorId;
+
+    @Column(name = "doctorfirstname")
+    private String doctorFirstName;
+
+    @Column(name = "doctorlastname")
+    private String doctorLastName;
+
+}
---
+++ src/main/java/edu/depaul/cdm/se/chicagomed/model/DoctorSpecialization.java
@@ -0,0 +1,32 @@
+package edu.depaul.cdm.se.chicagomed.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "doctorspecialization")
+@ToString
+public class DoctorSpecialization {
+
+    private static final long serialVersionUID = 2L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany(
+            mappedBy = "doctorId",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @ToString.Exclude
+    private List<Doctor> doctor_ID;
+
+    @Column(name = "specialization")
+    private String doctor_Specialization;
+}
