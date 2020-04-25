f04cfd70292f9c13e2b6e1cc13dac0c9224fffd8
ArbaazKhan1 <bazzy_94@yahoo.com>
2020-04-25 14:05:24 -0500

    Added java code for Appointment & Bill DB's

---
+++ src/main/java/edu/depaul/cdm/se/chicagomed/model/Bill.java
@@ -0,0 +1,56 @@
+package edu.depaul.cdm.se.chicagomed.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "bill")
+@ToString
+public class Bill {
+
+    private static final long serialVersionUID = 11L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @Column(unique = true)
+    @Size(max = 5)
+    private String billId;
+
+    @Column(name = "billamt")
+    private double billAmount;
+
+    @Column(name = "paidamt")
+    private double paidAmount;
+
+    @Column(name = "duedate")
+    private Date dueDate;
+
+    @Column(name = "billbalance")
+    private double billBalance;
+
+
+    @OneToMany(
+            mappedBy = "patientId",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @ToString.Exclude
+    private List<Patient> patient_Id;
+
+    @OneToMany(
+            mappedBy = "appointmentId",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @ToString.Exclude
+    private List<Appointment> appointment_ID;
+}
