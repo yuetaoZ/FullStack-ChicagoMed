beb3434a90f1f1bda955c11cfda9a21b27f07792
ArbaazKhan1 <bazzy_94@yahoo.com>
2020-05-02 16:29:05 -0500

    Added DB Repository interface code

---
+++ src/main/java/edu/depaul/cdm/se/chicagomed/repository/DoctorRepoistory.java
@@ -0,0 +1,7 @@
+package edu.depaul.cdm.se.chicagomed.repository;
+
+import edu.depaul.cdm.se.chicagomed.model.Doctor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DoctorRepoistory extends CrudRepository<Doctor,String> {
+}
---
+++ src/main/java/edu/depaul/cdm/se/chicagomed/repository/PatientDOBRepository.java
@@ -0,0 +1,8 @@
+package edu.depaul.cdm.se.chicagomed.repository;
+
+import edu.depaul.cdm.se.chicagomed.model.Bill;
+import edu.depaul.cdm.se.chicagomed.model.PatientDOB;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PatientDOBRepository extends CrudRepository<PatientDOB, String> {
+}
