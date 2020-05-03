9cd7097d713e93644fdce15882ce1a8677e2ce83
Kevin Looft <kdlooft@gmail.com>
2020-05-03 15:34:55 -0500

    Add LocationReview as NoSQL

---
+++ src/main/java/edu/depaul/cdm/se/chicagomed/model/LocationReview.java
@@ -0,0 +1,23 @@
+package edu.depaul.cdm.se.chicagomed.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("LocationReview")
+public class LocationReview {
+    @Id
+    public String locationId;
+    public String locationReview;
+
+    public LocationReview(String locationId, String review) {
+        this.locationId = locationId;
+        this.locationReview = review;
+    }
+
+
+    public String toString(){
+        return "locationId:" + locationId + "Review :" + locationReview;
+    }
+}
---
+++ src/main/java/edu/depaul/cdm/se/chicagomed/repository/LocationReviewRepository.java
@@ -0,0 +1,7 @@
+package edu.depaul.cdm.se.chicagomed.repository;
+
+import edu.depaul.cdm.se.chicagomed.model.LocationReview;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LocationReviewRepository extends CrudRepository<LocationReview, String> {
+}
