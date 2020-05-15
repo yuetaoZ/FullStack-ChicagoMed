f97fdaa1297a2c12f527b82d386a095164cc8bf3
Tommy O'Connell <thomas.m.oconnell@gmail.com>
2020-05-14 20:57:12 -0500

    Added new-appointment.html and updated PatientController and patient-header.html

--- src/main/java/edu/depaul/cdm/se/chicagomed/controller/PatientController.java
+++ src/main/java/edu/depaul/cdm/se/chicagomed/controller/PatientController.java
@@ -6,15 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PatientController {
-    @GetMapping("/patient-appointments")
+    @GetMapping("/patient-appointment")
     public String getPatientAppointments(Model model) {
-        return "patient-appointments";
+        return "patient-appointment";
     }
 
-//    @GetMapping("/doctor-appointment")
-//    public String getDoctorAppointment(Model model) {
-//        return "doctor-appointment";
-//    }
+    @GetMapping("/new-appointment")
+    public String getNewAppointment(Model model) {
+        return "new-appointment";
+    }
 
 //    @GetMapping("/doctor-schedule")
 //    public String getDoctorSchedule(Model model) {
