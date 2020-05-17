596c4f47bd53b689f57ab42dcf894ae7913b176c
Yuetao <yuetao.zhu.062984@gmail.com>
2020-05-17 12:50:10 -0500

    Added patient Info, patient historical/upcoming appointment details, billing details pages for Admin portal.

--- src/main/java/edu/depaul/cdm/se/chicagomed/controller/AdminController.java
+++ src/main/java/edu/depaul/cdm/se/chicagomed/controller/AdminController.java
@@ -11,8 +11,38 @@ public class AdminController {
         return "admin-appointments";
     }
 
-    @GetMapping("/admin-appointment")
-    public String getDoctorAppointment(Model model) {
-        return "admin-appointment";
+    @GetMapping("/admin-patientInfo")
+    public String getPatientInfo(Model model) {
+        return "admin-patientInfo";
+    }
+
+    @GetMapping("/admin-patientInfo-edit")
+    public String getPatientInfoEdit(Model model) {
+        return "admin-patientInfo-edit";
+    }
+
+    @GetMapping("/admin-patient-appointments-history")
+    public String getPatientAppointmentsHistory(Model model) {
+        return "admin-patient-appointments-history";
+    }
+
+    @GetMapping("/admin-patient-upcoming-appointments")
+    public String getPatientUpcomingAppointments(Model model) {
+        return "admin-patient-upcoming-appointments";
+    }
+
+    @GetMapping("/admin-patient-upcoming-appointment-details")
+    public String getPatientUpcomingAppointmentDetails(Model model) {
+        return "admin-patient-upcoming-appointment-details";
+    }
+
+    @GetMapping("/admin-patient-appointment-details")
+    public String getPatientAppointmentDetails(Model model) {
+        return "admin-patient-appointment-details";
+    }
+
+    @GetMapping("/admin-patient-billing-info")
+    public String getPatientBillingInfo(Model model) {
+        return "admin-patient-billing-info";
     }
 }
