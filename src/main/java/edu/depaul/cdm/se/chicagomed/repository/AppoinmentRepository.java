1195b9e93811f135ce107a1e80089fbb1f7cee91
Tommy O'Connell <thomas.m.oconnell@gmail.com>
2020-05-29 10:59:46 -0400

    Updated PatientController, patient-appointment.html and patient.html.  Also made changes to Location.java to add locationName.  Added findAllByPatient to AppoinmentRepository.

--- src/main/java/edu/depaul/cdm/se/chicagomed/repository/AppoinmentRepository.java
+++ src/main/java/edu/depaul/cdm/se/chicagomed/repository/AppoinmentRepository.java
@@ -2,6 +2,7 @@ package edu.depaul.cdm.se.chicagomed.repository;
 
 import edu.depaul.cdm.se.chicagomed.model.Appointment;
 import edu.depaul.cdm.se.chicagomed.model.Doctor;
+import edu.depaul.cdm.se.chicagomed.model.Patient;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,6 +11,8 @@ public interface AppoinmentRepository extends CrudRepository<Appointment, Long> 
     List<Appointment> findByAppointmentDate(String AppDate);
 
     List<Appointment> findAllByDoctor(Doctor doctor);
+
+    List<Appointment> findAllByPatient(Patient patient);
 
     @Override
     List<Appointment> findAll();
