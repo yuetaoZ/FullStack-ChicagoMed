bc1c70bb7aad8ff107b270783663cdd5fa5f40c8
Kevin Looft <kdlooft@gmail.com>
2020-06-08 07:31:03 -0500

    Add billing default data

---
+++ src/main/resources/db/migration/V1.05__add_billing_info.sql
@@ -0,0 +1,6 @@
+insert into bill(appointmentid, patientid, billid, billamt, paidamt, duedate, billbalance) values (1, 12348, 1, 250, 0, '2020-07-01 12:00:00', 250);
+insert into bill(appointmentid, patientid, billid, billamt, paidamt, duedate, billbalance) values (2, 12348, 2, 250, 0, '2020-07-01 12:00:00', 250);
+insert into bill(appointmentid, patientid, billid, billamt, paidamt, duedate, billbalance) values (3, 12348, 3, 250, 250, '2020-07-01 12:00:00', 0);
+insert into bill(appointmentid, patientid, billid, billamt, paidamt, duedate, billbalance) values (4, 12345, 4, 100, 0, '2020-07-01 12:00:00', 100);
+insert into bill(appointmentid, patientid, billid, billamt, paidamt, duedate, billbalance) values (5, 12345, 5, 150, 0, '2020-07-01 12:00:00', 150);
+
